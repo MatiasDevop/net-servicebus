0727961115db862760aa5fa5a38a1e9d2c98042b
Nestor Matias <solomatias321@gmail.com>
2021-07-08 12:06:24 +0200

    firs commit setup servicebus

---
+++ SBSender/Services/QueueService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBSender.Services
+{
+    public class QueueService
+    {
+        private readonly IConfiguration _config;
+        public QueueService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendMessageAsync<T>(T serviceBusMessage, string queueName)
+        {
+
+        }
+    }
+}
